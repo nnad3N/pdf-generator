cfac19a834e58c05ef7f36dfb2965fa911fbc710
Damian Struk <damianstruk@outlook.com>
2024-05-29 20:58:36 +0200

    Vitest config update

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { join } from "path";
 
 export default defineConfig({
   test: {
     alias: {
-      "@/": join(__dirname, "./src/"),
+      "@/": new URL("./src/", import.meta.url).pathname,
     },
     environment: "jsdom",
   },
